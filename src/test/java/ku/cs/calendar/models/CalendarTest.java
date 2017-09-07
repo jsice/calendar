7292641b952908619f051d9c759c5e61b7e480bf
jsice <w14chin@gmail.com>
2017-09-07 22:40:03 +0700

    add new test for calendar

--- src/test/java/ku/cs/calendar/models/CalendarTest.java
+++ src/test/java/ku/cs/calendar/models/CalendarTest.java
@@ -1,7 +1,9 @@
 package ku.cs.calendar.models;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 /**
  * Wiwadh Chinanuphandh
  * 5810400051
@@ -10,16 +12,31 @@ public class CalendarTest {
 
     private Calendar c;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         c = new Calendar();
     }
 
     @Test
     public void addAppointment() throws Exception {
-        Appointment ap = new Appointment(new Date(2560, 8, 31 ), 1, 1);
+        Appointment ap = new Appointment(new Date(31, 8, 2560 ), 1, 1);
         c.addAppointment(ap);
-        assert ap == c.getAppointments(31, 8, 2560).peek();
+        Assertions.assertEquals(ap, c.getAppointments(31, 8, 2560).peek());
+    }
+
+    @Test
+    public void hasAppointmentsOnDate() throws Exception {
+        Appointment ap = new Appointment(new Date(31, 8, 2560 ), 1, 1);
+        c.addAppointment(ap);
+        Assertions.assertEquals(c.hasAppointmentsOnDate(31,8,2560), true);
+    }
+
+    @Test
+    public void removeAppointment() throws Exception {
+        Appointment ap = new Appointment(new Date(31, 8, 2560 ), 1, 1);
+        c.addAppointment(ap);
+        c.removeAppointment(ap);
+        Assertions.assertEquals(c.hasAppointmentsOnDate(31,8,2560), false);
     }
 
 }
